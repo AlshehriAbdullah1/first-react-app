5abee9ff3fd335f72c79de385ceb16007245973f
AlshehriAbdullah1 <113243006+AlshehriAbdullah1@users.noreply.github.com>
2023-09-09 23:31:22 +0300

    adjustment

--- src/MyInput.js
+++ src/MyInput.js
@@ -1,8 +1,15 @@
+import { useState } from "react";
 export default function MyInput() {
+  const [myInputValue, setMyInputValue] = useState("");
+  function handleInputChange(event) {
+    setMyInputValue(event.target.value);
+    //console.log();
+    // setMyInputValue(value);
+  }
   return (
     <>
       <label>Your Name</label>
-      <input />
+      <input value={myInputValue} onChange={handleInputChange} />
     </>
   );
 }
